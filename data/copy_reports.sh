bceffe5dbc05f3983e17924487e52ed5fa81d8e1
Taylor Jadin <jadin.taylor@gmail.com>
2023-03-17 10:40:33 -0500

    Create copy_reports.sh

---
+++ data/copy_reports.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Config
+HOST=
+USER=
+
+# Setup
+PLUGIN_DATA_DIR=/home/"$USER"/public_html/wp-content/plugins/doooo-dashboard/data
+
+# Write php boilerplate
+echo -n `cat $PLUGIN_DATA_DIR/last-logins-template.php` > $PLUGIN_DATA_DIR/last_logins.php
+
+# Write report data
+echo -n `cat /root/"$HOST"_last_logins.csv` >> $PLUGIN_DATA_DIR/last_logins.php
+
+# Add final semicolon
+echo ";" >> $PLUGIN_DATA_DIR/last_logins.php
