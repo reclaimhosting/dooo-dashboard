ba7bf99ab80b44d35adf9988d43d90ddcba0f8ac
Taylor Jadin <jadin.taylor@gmail.com>
2023-03-17 12:41:17 -0500

    fix fix

--- data/copy_reports.sh
+++ data/copy_reports.sh
@@ -6,14 +6,14 @@ USER=`echo $SCRIPT_DIR | cut -d '/' -f 3`
 HOST=`echo $HOSTNAME | cut -d '.' -f 1`
 
 # Clear out file and write php boilerplate
-echo -n `cat $SCRIPT_DIR/template.php` > $SCRIPT_DIR/last_logins.php
-echo -n "$bar = \'LAST LOGIN DATE, USERNAME, EMAIL, PRIMARY DOMAIN, DISK USAGE, START DATE" > $PLUGIN_DATA_DIR/last_logins.php
+echo `cat $SCRIPT_DIR/template.php` > $SCRIPT_DIR/last-logins.php
+echo "$bar = \'LAST LOGIN DATE, USERNAME, EMAIL, PRIMARY DOMAIN, DISK USAGE, START DATE" > $SCRIPT_DIR/last-logins.php
 
 # Write report data
-echo -n `cat /root/"$HOST"_last_logins.csv` >> $SCRIPT_DIR/last_logins.php
+echo `cat /root/"$HOST"_last_logins.csv` >> $SCRIPT_DIR/last-logins.php
 
 # Add final semicolon
-echo "';" >> $SCRIPT_DIR/last_logins.php
+echo "';" >> $SCRIPT_DIR/last-logins.php
 
 # Make sure report is readable by WP
 chown $USER:$USER $SCRIPT_DIR/last_logins.php
