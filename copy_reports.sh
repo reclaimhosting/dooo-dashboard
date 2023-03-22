8d6eede947a6423f94d4107ec7702a216859a725
Taylor Jadin <jadin.taylor@gmail.com>
2023-03-22 16:40:49 -0500

    scp

--- copy_reports.sh
+++ copy_reports.sh
@@ -14,7 +14,7 @@ cat /root/"$HOST"_last_logins.csv >> $SCRIPT_DIR/data/last-logins.php
 # Get reports from other servers
 # Pass them as arguments when calling the script ex: `bash copy_reports.sh stateu2 stateu3 stateu4`
 # Make sure ssh keypairs are setup so this actually works without having to type passwords
-# Also make sure to run this at least once ot accept host keys
+# Also make sure to run this at least once to accept host keys
 for OTHER_HOST in "$@"
 do
 scp root@"$OTHER_HOST".reclaimhosting.com:/root/"$OTHER_HOST"_last_logins.csv /root/"$OTHER_HOST"_last_logins.csv
