ede06c52c76bdeb60aad1595a2dd55f58eb949fa
Taylor Jadin <jadin.taylor@gmail.com>
2023-03-22 16:04:38 -0500

    support multi-server DoOO

--- copy_reports.sh
+++ copy_reports.sh
@@ -11,6 +11,15 @@ cat $SCRIPT_DIR/template.txt > $SCRIPT_DIR/data/last-logins.php
 # Write report data
 cat /root/"$HOST"_last_logins.csv >> $SCRIPT_DIR/data/last-logins.php
 
+# Get reports from other servers
+# Pass them as arguments when calling the script ex: `bash copy_reports.sh stateu2 stateu3 stateu4`
+# Make sure ssh keypairs are setup so this actually works without having to type passwords
+for OTHER_HOST in "$@"
+do
+scp root@"$OTHER_HOST".reclaimhosting.com:/root/"$OTHER_HOST"_last_logins.csv /root/"$OTHER_HOST"_last_logins.csv
+tail -n+2 /root/"$OTHER_HOST"_last_logins.csv >> $SCRIPT_DIR/data/last-logins.php
+done
+
 # Add final semicolon
 echo "';" >> $SCRIPT_DIR/data/last-logins.php
 
